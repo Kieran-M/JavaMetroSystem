350f1414c7cd0bfb4f50e9ae32103b56e03f467d
William Kong <william.kong.2018@uni.strath.ac.uk>
2021-02-17 17:48:58 +0000

    Implemented UML Diagram into the Classes

--- src/main/java/org/group8/bostonmetrosystem/BostonMetro.java
+++ src/main/java/org/group8/bostonmetrosystem/BostonMetro.java
@@ -1,6 +1,34 @@
 package org.group8.bostonmetrosystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BostonMetro {
+    private Station station;
+    private String currentStation;
+    private String destination;
+    private ArrayList<String> route;
 
+    public String getStation(){
+        currentStation = station.getName();
+        return currentStation;
+    }
 
+    //Get station from user's input from the GUI
+    public void setStation(String current){
+        currentStation = current;
+    }
+
+    //Get destination from user's input from the GUI
+    public void setDestination(String dest){
+        destination = dest;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public List<String> getRoute(){
+        return route;
+    }
 }
--- src/main/java/org/group8/bostonmetrosystem/Station.java
+++ src/main/java/org/group8/bostonmetrosystem/Station.java
@@ -3,10 +3,24 @@ package org.group8.bostonmetrosystem;
 public class Station {
 
     private int id;
-    private String label;
+    private String station;
+    private String metroLine;
 
-    Station(int id, String label) {
+    Station(int id, String station, String metroLine) {
         this.id = id;
-        this.label = label;
+        this.station = station;
+        this.metroLine = metroLine;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public String getName(){
+        return station;
+    }
+
+    public String getMetroLine(){
+        return metroLine;
     }
 }
