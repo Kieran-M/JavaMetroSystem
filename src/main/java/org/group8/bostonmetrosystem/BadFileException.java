3d9d7d562af695114796820bff52cd84713de60f
dandyrow <daniel@daniellowry.co.uk>
2021-02-17 20:05:16 +0000

    Modified BadFileException to use exception methods

--- src/main/java/org/group8/bostonmetrosystem/BadFileException.java
+++ src/main/java/org/group8/bostonmetrosystem/BadFileException.java
@@ -1,11 +1,7 @@
 package org.group8.bostonmetrosystem;
 
 public class BadFileException extends Exception {
-
-    private String error;
-
-    BadFileException(String errorMessage) {
-        error = errorMessage;
-        System.out.println("Error: " + error);
+    BadFileException(String message) {
+        super(message);
     }
 }
