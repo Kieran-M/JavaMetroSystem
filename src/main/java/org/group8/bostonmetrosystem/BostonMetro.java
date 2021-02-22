c6af38de35487135a0c80ddf533051b42fff933d
William Kong <william.kong.2018@uni.strath.ac.uk>
2021-02-22 17:04:14 +0000

    Read text file to get station information and created user input for current and destination station

--- src/main/java/org/group8/bostonmetrosystem/BostonMetro.java
+++ src/main/java/org/group8/bostonmetrosystem/BostonMetro.java
@@ -1,7 +1,9 @@
 package org.group8.bostonmetrosystem;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -25,38 +27,37 @@ public class BostonMetro {
     private ArrayList<String> route;
 
     /**
-     * Gets the current station
-     * @return the station name
+     * Prints the current station
      */
-    public String getStation(){
-        currentStation = station.getName();
-        return currentStation;
+    public void getStation(){
+        System.out.println("Your current Station is: " + currentStation);
     }
 
     /**
      * Sets the current station
-     * @param name the station to be set
      */
     //Get station from user's input from the GUI
-    public void setStation(String name){
-        currentStation = name;
+    public void setStation(){
+        System.out.println("Please enter your current Station");
+        Scanner name = new Scanner(System.in);
+        currentStation = name.nextLine();
     }
 
     /**
      * Sets the destination station
-     * @param dest the destination
      */
     //Get destination from user's input from the GUI
-    public void setDestination(String dest){
-        destination = dest;
+    public void setDestination(){
+        System.out.println("Please enter your destination");
+        Scanner name = new Scanner(System.in);
+        destination = name.nextLine();
     }
 
     /**
-     * Gets the destination name
-     * @return destination station name
+     * Prints the destination name
      */
-    public String getDestination(){
-        return destination;
+    public void getDestination(){
+        System.out.println("Your destination Station is: " + destination);
     }
 
     /**
@@ -66,4 +67,29 @@ public class BostonMetro {
     public List<String> getRoute(){
         return route;
     }
+
+    /**
+     * Constructs a new instance MetroMapParser to read the text file and return
+     * information about each station
+     * @throws BadFileException if you can not read the text file
+     */
+    public void readMap() throws BadFileException{
+        try {
+            MetroMapParser read = new MetroMapParser("src/main/resources/bostonmetro.txt");
+            ArrayList<ArrayList<String>> stations = read.generateGraphFromFile();
+            System.out.println(stations);
+        } catch (IOException e) {
+            throw new BadFileException("Can not read text file");
+        }
+    }
+
+    public static void main(String[] args) throws BadFileException {
+        BostonMetro Metro = new BostonMetro();
+        Metro.readMap();
+        Metro.setStation();
+        Metro.setDestination();
+        Metro.getStation();
+        Metro.getDestination();
+    }
+
 }
