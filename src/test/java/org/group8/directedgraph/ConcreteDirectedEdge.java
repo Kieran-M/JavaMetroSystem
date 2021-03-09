8263b1b3577d6b7ad4ab2e1c3e6a13474b3a92a7
dandyrow <daniel@daniellowry.co.uk>
2021-03-09 22:11:19 +0000

    Finished multigraph tests

--- src/test/java/org/group8/directedgraph/ConcreteDirectedEdge.java
+++ src/test/java/org/group8/directedgraph/ConcreteDirectedEdge.java
@@ -1,5 +1,9 @@
 package org.group8.directedgraph;
 
+import org.group8.bostonmetrosystem.Station;
+
+import java.util.Objects;
+
 public class ConcreteDirectedEdge<V> implements DirectedEdge<V> {
 
     private V sourceVertex;
@@ -29,5 +33,31 @@ public class ConcreteDirectedEdge<V> implements DirectedEdge<V> {
     @Override
     public V getTargetVertex() {
         return targetVertex;
+    }
+
+    /**
+     * Overrides the equals method to allow for proper comparison between edges.
+     * @param o The object to compare to this object
+     * @return {@code true} if the object is the same as this object
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ConcreteDirectedEdge<?> that = (ConcreteDirectedEdge<?>) o;
+        return getSourceVertex().equals(that.getSourceVertex()) && getTargetVertex().equals(that.getTargetVertex());
+    }
+
+    /**
+     * Overrides the {@code hashCode} method for proper comparison between tracks.
+     * @return the hashcode of the object
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSourceVertex(), getTargetVertex());
     }
 }
--- src/test/java/org/group8/directedgraph/MultiGraphTest.java
+++ src/test/java/org/group8/directedgraph/MultiGraphTest.java
@@ -79,8 +79,17 @@ public class MultiGraphTest {
     }
 
     @Test
-    void removingVertexWithEdgeRemovesEdge() {
+    void removingVertexWithEdgeRemovesEdge() throws VertexNotFoundException {
+        DirectedGraph<String, ConcreteDirectedEdge<String>> graph = new MultiGraph<>();
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addEdge(new ConcreteDirectedEdge<>("v1", "v2"));
 
+        Assertions.assertEquals(2, graph.getNumVertices());
+        Assertions.assertEquals(1, graph.getNumEdges());
+        Assertions.assertTrue(graph.removeVertex("v1"));
+        Assertions.assertEquals(1, graph.getNumVertices());
+        Assertions.assertEquals(0, graph.getNumEdges());
     }
 
     @Test
@@ -116,12 +125,30 @@ public class MultiGraphTest {
     }
 
     @Test
-    void isAdjacent() {
-
+    void isAdjacent() throws VertexNotFoundException {
+        DirectedGraph<String, ConcreteDirectedEdge<String>> graph = new MultiGraph<>();
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        Assertions.assertTrue(graph.addEdge(new ConcreteDirectedEdge<>("v1","v2")));
+        Assertions.assertTrue(graph.isAdjacent("v1","v2"));
+        Assertions.assertFalse(graph.isAdjacent("v2","v1"));
     }
 
     @Test
-    void getAdjacentVerticies() {
+    void getAdjacentVerticies() throws VertexNotFoundException {
+        DirectedGraph<Integer, ConcreteDirectedEdge<Integer>> graph = new MultiGraph<>();
+        List<Integer> vertices = new ArrayList<>();
 
+        for (int i = 0; i < 10; i++) {
+            vertices.add(i);
+        }
+
+        graph.addVertices(vertices);
+        graph.addEdge(new ConcreteDirectedEdge<>(1,2));
+        graph.addEdge(new ConcreteDirectedEdge<>(1,3));
+        graph.addEdge(new ConcreteDirectedEdge<>(1,4));
+        List<Integer> adjacent = graph.getAdjacent(1);
+        Assertions.assertTrue(adjacent.contains(2) && adjacent.contains(3) && adjacent.contains(4));
+        Assertions.assertFalse(adjacent.contains(1));
     }
 }
